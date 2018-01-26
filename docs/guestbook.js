3c93d0ce9d96f05a5c84132f9ec7035e56708035
vijayjeevaraj <35842350+vijayjeevaraj@users.noreply.github.com>
2018-01-26 10:01:55 -0600

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -15,7 +15,7 @@ const guestbook = {
   add(name, email, comment) {
     console.log('Sending', name, email, comment)
     return $.ajax({
-      type: 'PUT',
+      type: 'POST',
       url: `${apiUrl}/entry`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
